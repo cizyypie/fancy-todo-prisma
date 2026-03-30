76bdaa512742678ca29e60bb26eea0a8e9b744d6
Joan <joanitatimbinp@gmail.com>
2026-03-30 22:32:43 +0700

    add validation feature (zod)

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,49 +1,51 @@
 import * as todoService from "../services/todoService.js";
+import { createTodoSchema, updateTodoSchema } from "../validations/todoValidation.js";
+import { handleError } from "../utils/errorHandler.js";
 
 export const getAllTodo = async (req, res) => {
   try {
     const todo = await todoService.getAllTodo();
     res.status(200).json({ data: todo });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    handleError(res, error);
   }
 };
 
 export const getTodoById = async (req, res) => {
   try {
-    const todo = await todoService.getTodoById(req.params.id); 
-    
+    const todo = await todoService.getTodoById(req.params.id);
     if (!todo) return res.status(404).json({ message: "Todo not found" });
     res.status(200).json({ data: todo });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    handleError(res, error);
   }
 };
 
 export const createTodo = async (req, res) => {
   try {
-    const todo = await todoService.createTodo(req.body);
+    const validatedData = createTodoSchema.parse(req.body);
+    const todo = await todoService.createTodo(validatedData);
     res.status(201).json({ data: todo });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    handleError(res, error);
   }
 };
 
 export const updateTodo = async (req, res) => {
   try {
-    const todo = await todoService.updateTodo(req.params.id, req.body);
-    if (!todo) return res.status(404).json({ message: "Todo not found" });
+    const validatedData = updateTodoSchema.parse(req.body);
+    const todo = await todoService.updateTodo(req.params.id, validatedData);
     res.status(200).json({ data: todo });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    handleError(res, error);
   }
 };
 
 export const deleteTodo = async (req, res) => {
   try {
-    await todoService.deleteTodo(req.params.id); 
+    await todoService.deleteTodo(req.params.id);
     res.status(200).json({ message: "Todo Deleted" });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    handleError(res, error);
   }
 };
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,35 @@
 import * as userService from "../services/userService.js";
+import { createUserSchema, updateUserSchema } from "../validations/userValidation.js";
+import { handleError } from "../utils/errorHandler.js";
+
+export const createUser = async (req, res) => {
+  try {
+    const validatedData = createUserSchema.parse(req.body);
+
+    const user = await userService.createUser(validatedData);
+    res.status(201).json({ data: user });
+ } catch (error) {
+  handleError(res, error);
+}
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const validatedData = updateUserSchema.parse(req.body);
+    const user = await userService.updateUser(req.params.id, validatedData);
+    res.status(200).json({ data: user });
+  } catch (error) {
+  handleError(res, error);
+}
+};
 
 export const getAllUsers = async (req, res) => {
   try {
     const user = await userService.getAllUsers();
     res.status(200).json({ data: user });
   } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+  handleError(res, error);
+}
 };
 
 export const getUserById = async (req, res) => {
@@ -16,26 +39,8 @@ export const getUserById = async (req, res) => {
     if (!user) return res.status(404).json({ message: "User not found" });
     res.status(200).json({ data: user });
   } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-export const createUser = async (req, res) => {
-  try {
-    const user = await userService.createUser(req.body);
-    res.status(201).json({ data: user });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-export const updateUser = async (req, res) => {
-  try {
-    const user = await userService.updateUser(req.params.id, req.body);
-    res.status(200).json({ data: user });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+  handleError(res, error);
+}
 };
 
 export const deleteUser = async (req, res) => {
@@ -43,6 +48,6 @@ export const deleteUser = async (req, res) => {
     await userService.deleteUser(req.params.id); 
     res.status(200).json({ message: "User Deleted" });
   } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+  handleError(res, error);
+}
 };
